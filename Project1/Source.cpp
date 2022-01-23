adfc12039d89d4e236fde33c0fca5de51cc59547
wheazzydev <wheazzydev@gmail.com>
2022-01-23 18:19:13 +0500

    .

---
+++ Project1/Source.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	/*
+	double a = 8.5;
+	float b = 7.4;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+	*/
+
+
+
+
+	// HAQIQIY SONLAR
+
+	//floor ceil trunc round
+
+	/*
+	cout << "floor(a) = " << floor(a) << endl; //8
+	cout << "floor(b) = " << floor(b) << endl; //7
+
+	cout << "ceil(a) = " << ceil(a) << endl; //9
+	cout << "ceil(b) = " << ceil(b) << endl; //8
+
+	cout << "trunc(a) = " << trunc(a) << endl; //8  rm num from .
+	cout << "trunc(b) = " << trunc(b) << endl; //7
+
+	cout << "round(a) = " << round(a) << endl; //9
+	cout << "round(b) = " << round(b) << endl; //7
+	*/
+
+
+
+
+
+	//MANTIQIY AMALLAR
+
+	// !, !=, ==, >=, <=, >, <, &&, ||
+
+	/*
+	bool m, n;
+	m = 7 > 7;
+	n = 8 <= 8;
+
+	cout << m << endl;
+	cout << n << endl;
+
+	cout << (true && true) << endl;
+	cout << (false && true) << endl;
+	cout << (true && false) << endl;
+	cout << (false && false) << endl;
+
+
+	cout << (true || true) << endl;
+	cout << (false || true) << endl;
+	cout << (true || false) << endl;
+	cout << (false || false) << endl;
+	*/
+
+
+	//SHART OPERATORLARI
+
+	char n;
+	cout << "N = "; cin >> n;
+
+	if (n == 'A') cout << "Hello" << endl;
+	else cout << "Bye" << endl;
+
+	/*
+	if (n > 0) {
+		cout << "Kiritilgan son musbat" << endl;
+		cout << "Siz " << n << " sonini kiritdingiz" << endl;
+	}
+	else if (n == 0) cout << "0 soni musbat ham , manfiy ham emas!!!" << endl;
+	else {
+		cout << "Kiritilgan son manfiy" << endl;
+		cout << "Siz " << n << " sonini kiritdingiz" << endl;
+	}
+	*/
+
+
+	system("pause");
+	return 0;
+}
