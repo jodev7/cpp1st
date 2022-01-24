6de3649fed1e7c005950802c3e44841b650f2961
wheazzydev <wheazzydev@gmail.com>
2022-01-24 10:49:49 +0500

    min max algoritmi

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -8,6 +8,23 @@ using namespace std;
 
 int main() {
 	
+	int min, max, n, number;
+
+	cout << "N ga qiymat bering: "; cin >> n;
+
+	cout << "1-son: "; cin >> number;
+
+	min = number; max = number;
+
+	for (int i = 0; i < n-1; i++) {
+		cout << i + 2 << "-son:"; cin >> number;
+
+		if (number < min) min = number;
+		if (number > max) max = number;
+	}
+
+	cout << "Min: " << min << endl;
+	cout << "Max: " << max << endl;
 	
 
 	system("pause");
