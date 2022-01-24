bb4d6181c49f711c25578f7bb439b512ebfa8268
wheazzydev <wheazzydev@gmail.com>
2022-01-24 09:58:32 +0500

    .

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 
 int main() {
+	/*
 	int n;
 	cout << "Hafta kuni sonni yozing = "; cin >> n;
 
-	/*
+	
 	switch (n) {
 	case 1: cout << "Dushanba" << endl; break;
 	case 2: cout << "Seshanba" << endl; break;
@@ -19,22 +20,25 @@ int main() {
 	case 7: cout << "Yakshanba" << endl; break;
 	default: cout << "Bunday hafta kuni mavjud emas" << endl;
 	}
-	*/
+	
 
 	
-	/*
+	
 	switch (n) {
 	case 1: case 2: case 3: cout << "Qish" << endl; break;
 	case 4: case 5: case 6: cout << "Bahor" << endl; break;
 	case 7: case 8: case 9: cout << "Yoz" << endl; break;
 	case 10: case 11: case 12: cout << "Kuz" << endl; break;
 	}
-	*/
+	
 	
 	//n > 0 ? cout << "manfiy" << endl : cout << "musbat" << endl;
 
 	n % 2 ==0 ? cout << "juft son" << endl : cout << "toq son" << endl;
+	*/
 
+
+	//WHILE TAKRORLASH OPERATORI
 	system("pause");
 	return 0;
 }
