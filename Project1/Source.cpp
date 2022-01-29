d4ccff4354302c0ce2f95d01e50f1df646d675d3
wheazzydev <wheazzydev@gmail.com>
2022-01-29 14:08:22 +0500

    class oop

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,19 +1,22 @@
-// Function & Array
+// Class - OOP
 
 #include <iostream>
 
 using namespace std;
 
-void printArray(int arr[], int size) {
-	for (int i = 0; i < size; i ++ ) {
-		cout << ++arr[i] << endl;
+//public, private, protected
+class Person {
+public:
+	void sayHi() {
+		cout << "Hello" << endl;
 	}
-}
+};
 
 int main() {
 
-	int myarr[3] = { 1,2,3 };
-	printArray(myarr, 3);
+	Person Yusuf;
+
+	Yusuf.sayHi();
 	
 	system("pause");
 	return 0;
