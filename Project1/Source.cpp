5c3f3387acdbef6a3e8fab818d2acb1ad8007633
wheazzydev <wheazzydev@gmail.com>
2022-01-29 14:36:05 +0500

    data hiding

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,25 +1,39 @@
-// Abstraction - OOP
+// Encapsulation (data hiding)
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-//
-class BankAccount {
-	int balance = 1000;
+class BankAccaount {
+private:
+	int balance = 1000; //$
+	string passport = "AC0000";
+
 public:
-	int myBalance() {
-		return balance * 9500;
+	void deposit(string s, int b) {
+		if (s == passport) {
+			balance += b;
+		}
+	}
+
+	void withdraw(string s, int b) {
+		if (s == passport) {
+			balance -= b;
+		}
 	}
 };
 
+
 int main() {
 
-	BankAccount k;
+	BankAccaount Yusuf;
 
-	cout << k.myBalance() << endl;
+	Yusuf.deposit("AC0000", 4000);
+	Yusuf.withdraw("AC0000", 2500);
 	
-	
+	cout << Yusuf.balance << endl;
+
 	system("pause");
 	return 0;
 }
