0efe618bf581d045c50c7d80c85aa3cf098ae700
wheazzydev <wheazzydev@gmail.com>
2022-01-29 13:24:42 +0500

    .

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,22 +1,36 @@
-// Recursion function
+// Fibonacci numbers
 
 #include <iostream>
 
 using namespace std;
 
-int fact(int n) {
-	if (n == 1)
+int fib(int n) { //6
+	if (n <= 2)
 		return 1;
 	else
-		return n * fact(n - 1);
+		return fib(n - 1) + fib(n - 2); // fib(5) + fib(4)
 }
+
+/*
+fib(5) = fib(4) + fib(3)
+fib(4) = fib(3) + fib(2)
+fib(3) = fib(2) + fib(1)
+fib(2) = 1
+fib(1) = 1
+*/
+
 
 int main() {
 
-	int k = 5;
+	//cout << fib(6) << endl;
 
-	cout << fact(k) << endl;
-	
+	int n;
+
+	cout << "sonni kiriting :"; cin >> n;
+
+	for (int i = 0; i <= n; i++) {
+		cout << fib(i) << " ";
+	}
 
 	system("pause");
 	return 0;
