c9a9fb07568f72a1a81cb7de7c5f3fe7e91f7015
wheazzydev <wheazzydev@gmail.com>
2022-01-29 13:43:48 +0500

    .

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,37 +1,20 @@
-// Fibonacci numbers
+// Function & Array
 
 #include <iostream>
 
 using namespace std;
 
-int fib(int n) { //6
-	if (n <= 2)
-		return 1;
-	else
-		return fib(n - 1) + fib(n - 2); // fib(5) + fib(4)
+void printArray(int arr[], int size) {
+	for (int i = 0; i < size; i ++ ) {
+		cout << ++arr[i] << endl;
+	}
 }
-
-/*
-fib(5) = fib(4) + fib(3)
-fib(4) = fib(3) + fib(2)
-fib(3) = fib(2) + fib(1)
-fib(2) = 1
-fib(1) = 1
-*/
-
 
 int main() {
 
-	//cout << fib(6) << endl;
-
-	int n;
-
-	cout << "sonni kiriting :"; cin >> n;
-
-	for (int i = 0; i <= n; i++) {
-		cout << fib(i) << " ";
-	}
-
+	int myarr[3] = { 1,2,3 };
+	printArray(myarr, 3);
+	
 	system("pause");
 	return 0;
 }
